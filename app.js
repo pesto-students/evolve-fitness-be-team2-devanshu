90dfd1064717ef5980c72202aefffb2792a797a8
vipin98 <pvipin907@gmail.com>
2022-09-11 10:36:06 +0530

    cors err fix

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ mongoose
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 //My Routes
 
